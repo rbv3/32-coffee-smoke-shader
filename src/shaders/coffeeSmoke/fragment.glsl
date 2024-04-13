efd2d6d0a02435ecc11728b24cdf88b3e7d93048
Rodrigo Valenca <rvalenca@amazon.com>
2024-04-12 21:13:48 -0300

    fix edges of animation

--- src/shaders/coffeeSmoke/fragment.glsl
+++ src/shaders/coffeeSmoke/fragment.glsl
@@ -9,9 +9,15 @@ void main() {
     smokeUv.y *= 0.3;
     smokeUv.y -= uTime * 0.04;
 
-    float smoke = texture(uPerlinTexture, smokeUv).r;
+     float smoke = texture(uPerlinTexture, smokeUv).r;
+    smoke = smoothstep(0.4, 1.0, smoke);
 
-    gl_FragColor = vec4(vec3(1.0), smoke);
+    smoke *= smoothstep(0.0, 0.1, vUv.x);
+    smoke *= smoothstep(1.0, 0.9, vUv.x);
+    smoke *= smoothstep(0.0, 0.1, vUv.y);
+    smoke *= smoothstep(1.0, 0.4, vUv.y);
+
+    gl_FragColor = vec4(0.4, 0.3, 0.2, smoke);
     
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
